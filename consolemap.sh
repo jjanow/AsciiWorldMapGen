2030e0ad80f96c74e70a8fae068d668cd3fa8eec
Jimmy Janow <10904006+jjanow@users.noreply.github.com>
2025-06-04 18:27:17 -0400

    example DF map and script to test with

---
+++ consolemap.sh
@@ -0,0 +1,1 @@
+python3 world_generator.py ascii --width 100 --height 40 --seed 42 --scale 0.1
---
+++ filemap.sh
@@ -0,0 +1,1 @@
+python3 world_generator.py graphics --width 10 --height 5 --seed 42 --scale 0.1 --output test_map.pn
